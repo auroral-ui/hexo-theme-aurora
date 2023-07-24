604792dd325570b7dbb9bab8a00ae7f3fc9dadc9
Benny Guo <bennyxguo6@gmail.com>
2023-07-24 12:17:38 +0800

    chore: change commit lint footer-max-length

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'body-max-line-length': [0, 'always', 'infinity']
+    'body-max-line-length': [0, 'always', 'infinity'],
+    'footer-max-line-length': [0, 'always', 'infinity']
   }
-};
+}
